f6671f93403d46965a5ef35acbf550e890602a53
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-06 15:23:33 -0700

    refactors tests to use table based tests

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -1,7 +1,6 @@
 package structs
 
 import (
-	"math"
 	"testing"
 )
 
@@ -22,23 +21,17 @@ func TestRectanglePerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-
-		got := shape.Area()
-
-		checkExpected(t, got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{Length: 2.0, Width: 4.0}
-		checkArea(t, rectangle, 8.0)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		checkExpected(t, got, tt.want)
+	}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := math.Pi * 100
-
-		checkArea(t, circle, want)
-	})
 }
