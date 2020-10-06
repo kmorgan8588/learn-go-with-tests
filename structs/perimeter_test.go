17d9a23c28b32ce93dd99a6ecc434b0f2f3bbb3e
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-06 14:59:57 -0700

    Adds basic Area func

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -2,11 +2,24 @@ package structs
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
+func checkExpected(t *testing.T, got, want float64) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %.2f and wanted %.2f", got, want)
 	}
+}
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	checkExpected(t, got, want)
+}
+
+func TestArea(t *testing.T) {
+	got := Area(2, 4)
+	want := 8.0
+
+	checkExpected(t, got, want)
+
 }
