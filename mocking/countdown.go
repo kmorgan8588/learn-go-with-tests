ed34673bf37b8503deed81c0b1ad2a8795d02396
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 12:12:38 -0700

    configures mock Sleeper struct to check order of operations and correct operations when calling Countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,15 +3,30 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+//
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
+	sleeper.Sleep()
 	fmt.Fprintf(out, finalWord)
 
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,18 +2,48 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 func TestCoundown(t *testing.T) {
-	buffer := &bytes.Buffer{}
+	t.Run("correct output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationSpy{})
 
-	Countdown(buffer)
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
 
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{sleep, write, sleep, write, sleep, write, sleep, write}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
