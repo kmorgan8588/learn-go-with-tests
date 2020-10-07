5a40992a1b32c7ac85d6e481cea91f8af9c49a4b
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 15:23:28 -0700

    implements channel usage in walk

--- reflection/walk.go
+++ reflection/walk.go
@@ -24,6 +24,10 @@ func walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walk(val.MapIndex(key).Interface(), fn)
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
 	}
 }
 
