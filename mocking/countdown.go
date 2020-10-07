8f93650113e027402cbb23ebebb3f8e3e7e86cc7
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 11:53:10 -0700

    updated Countdown to countdown and print each value until Go!

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -5,6 +5,13 @@ import (
 	"io"
 )
 
-func Countdown(w io.Writer) {
-	fmt.Fprintf(w, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprintf(out, finalWord)
+
 }
