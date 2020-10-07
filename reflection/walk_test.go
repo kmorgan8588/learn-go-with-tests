21cd96fb9761ac98920fccb4c602bc81ea807192
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 14:26:57 -0700

    updates tests to use BlockTests and introduces ability to have multiple string fields be added instead of the first one

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -1,26 +1,46 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	t.Run("take a walk", func(t *testing.T) {
-		expected := "Chris"
-		var got []string
 
-		x := struct {
-			Name string
-		}{expected}
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
 
-		walk(x, func(input string) {
-			got = append(got, input)
+	for _, test := range cases {
+
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
 		})
-
-		if len(got) != 1 {
-			t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
-		}
-
-		if got[0] != expected {
-			t.Errorf("got %q, want %q", got[0], expected)
-		}
-	})
+	}
 }
