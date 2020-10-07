5fb94684d2b9ed57f5235bed3a2634e1c5e75105
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 14:18:08 -0700

    uses unsafe reflection to pass base test case

--- reflection/walk.go
+++ reflection/walk.go
@@ -1,5 +1,9 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe...")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
