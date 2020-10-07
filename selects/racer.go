43f95ba6187db9cc7bec1be4d0ffcf27b7a8f04c
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-07 13:29:12 -0700

    implements Racer to see which of two requests is quicker

--- selects/racer.go
+++ selects/racer.go
@@ -1,5 +1,22 @@
 package selects
 
-func Racer(url1, url2 string) string {
-	return ""
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
