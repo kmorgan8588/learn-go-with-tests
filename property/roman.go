912d0450cd59d93bfbff6c1c43bdfaea2f82f6b4
Kyle Morgan <kmorgan8588@gmail.com>
2020-10-08 14:52:00 -0700

    refactor ConvertToArabic and add type and methods to assist

--- property/roman.go
+++ property/roman.go
@@ -18,6 +18,18 @@ func (r RomanNumerals) ValueOf(symbols ...byte) int {
 	return 0
 }
 
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+
+	return false
+}
+
 var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
@@ -46,27 +58,30 @@ func ConvertToRoman(num int) string {
 	return result.String()
 }
 
-func ConvertToArabic(roman string) int {
-	total := 0
-
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i]
-
-		if couldBeSubtractive(i, symbol, roman) {
-			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
-				total += value
-				i++ //used the character, so we iterate past it
-			} else {
-				total += allRomanNumerals.ValueOf(symbol)
-			}
-		} else {
-			total += allRomanNumerals.ValueOf(symbol)
-		}
+func ConvertToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
 	}
-	return total
+	return
 }
 
-func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
-	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
-	return index+1 < len(roman) && isSubtractiveSymbol
+func isSubtractive(currentSymbol uint8) bool {
+	return currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+}
+
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{byte(symbol), byte(w[i+1])})
+			i++
+		} else {
+			symbols = append(symbols, []byte{byte(symbol)})
+		}
+	}
+	return
 }
